83238c7b1d79066298d105aa507d8958cf32b5fc
Oswyn Deloras <oswyndel@su-h4.msk.21-school.ru>
2023-07-01 11:53:06 +0300

    print field

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void input(int ***matrix);  // заполнение поля нулями
+void output(int **matrix);  // вывод поля
+void free_matrix(int ***matrix);
+
+
+enum Sizes {
+    WIDTH = 80,
+    HEIGTH = 25
+};
+
+
+int main() {
+    // printf("%d", WIDTH);
+    int **field = calloc(HEIGTH, sizeof(int*));
+    input(&field);
+    output(field);
+    free_matrix(&field);
+   
+}
+
+void input(int ***matrix) {
+    for (short int row = 0; row < HEIGTH; row++) {
+        (*matrix)[row] = calloc(WIDTH, sizeof(int));
+    }
+}
+
+
+void output(int **matrix) {
+    for (short int row = 0; row < HEIGTH; row++) {
+        for (short int col = 0; col < WIDTH; col++) {
+            printf("%d ", matrix[row][col]);
+        }
+        putchar('\n');
+    }
+}
+
+void free_matrix(int ***matrix) {
+    for (short int row = 0; row < HEIGTH; row++) {
+        free((*matrix)[row]);
+    }
+    free(*matrix);
+}
